88b0ef0a32d913428ca97fa104f7aed3e748d726
jehan ichhaporia <jehan_ichhaporia@hotmail.com>
2025-10-22 21:59:05 +0530

    add

--- hashcode_importPackages.dart
+++ hashcode_importPackages.dart
@@ -1,19 +1,31 @@
 ///2 same objects should have same hashcode
+///Equatible package will override hashcode for your created class
+///With this override for equality operator is not required
+///with equatable package the override stringify getter is used to automatically 
+///take care of toString() method overriding
+///Equatable in 4 steps
+///1-Install in pubspec.yaml 
+///2-Add extends Equatable to our classes
+///3-Override props variable
+///4-Override stringify and return true
+///Equatable should be used only with immutable classes
 
-class Point {
+
+
+
+import 'package:equatable/equatable.dart';
+
+class Point extends Equatable{
   Point(this.x, this.y);
   final int x;
   final int y;
 
   @override
-  String toString() => 'Point($x,$y)';
-
- 
+  List<Object?> get props =>[x,y];
 
   @override
-  bool operator ==(covariant Point other) {
-    return x == other.x && y == other.y;
-  }
+  bool? get stringify => true;
+  
 
   Point operator +(Point other) {
     return Point(x + other.x, y + other.y);
@@ -25,6 +37,8 @@ class Point {
 }
 
 void main() {
+  print(Point(0, 0)==Point(0, 0));
+  print(Point(1, 1));
   print(Point(1, 1) + Point(2, 0));
   print(Point(2, 1)*5);
 }
